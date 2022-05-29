b481fcf6a3a9b2be63ee934fe89bdfad389eea8c
ThirdBlood <M1150562056@outlook.com>
2022-05-29 12:35:59 +0800

    完善权限系统

--- server/settings.py
+++ server/settings.py
@@ -6,7 +6,7 @@ ip_address = '127.0.0.1'  # 服务器建立的ip地址，通常为localhost（12
 network_port = 8080  # 服务器的端口，内网穿透的时候要填写本地端口为此
 default_chatting_room = 'Lhat! Chatting Room'  # 默认聊天室名称
 password = '123456'  # 服务器的密码，用于平常用户权限操作的验证
-root_password = '12345678'  # 服务器的管理员密码，用于管理员权限操作的验证
+root_password = '12345678'  # 服务器的管理员密码，用于管理员权限操作的验证，不能有空格！
 
 log = True  # 是否记录日志
 record = True  # 是否记录聊天记录
